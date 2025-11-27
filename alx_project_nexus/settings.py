dbf6fb69d2c243db4eb648fe9450fadcaa0d9c8f
Nkamenyin <valentineeyet01@gmail.com>
2025-11-27 11:38:09 +0100

    Integrated Swagger documentation for API endpoints

--- alx_project_nexus/settings.py
+++ alx_project_nexus/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Only for deployment
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,6 +40,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
